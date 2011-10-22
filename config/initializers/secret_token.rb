c32429ab524a9fa0390b34d8354cb7357a6cd2b6
Josh Begin <jbegin@ipipeline.com>
2011-10-22 17:14:20 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fam3site::Application.config.secret_token = 'f276ccc49801ad3de9ebd2420008b49369fd06b83468e32eab7210e5ffafb9c962bc2c1143dea1ffbed543e6c2882f6dacc106b619575088dd6211c06705d036'
